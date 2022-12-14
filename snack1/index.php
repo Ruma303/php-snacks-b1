1740822504626e851a242b96c74a561b91be6393
Ruma <matteoh8@hotmail.it>
2022-12-14 18:43:15 +0100

    snack1 ok

--- snack1/index.php
+++ snack1/index.php
@@ -24,11 +24,31 @@
             'homePoints' => 69,
             'hostPoints' => 54,
         ],
+        [
+            'homeTeam' => 'VY Canis Majoris',
+            'hostTeam' => 'KY Cygni',
+            'homePoints' => 23,
+            'hostPoints' => 21,
+        ],
+        [
+            'homeTeam' => 'RSGC1-F01',
+            'hostTeam' => 'IRC-10414',
+            'homePoints' => 34,
+            'hostPoints' => 39,
+        ],
+        [
+            'homeTeam' => 'CK Carinae',
+            'hostTeam' => 'BI Cygni',
+            'homePoints' => 18,
+            'hostPoints' => 2,
+        ],
+        [
+            'homeTeam' => 'Stephenson 2-18',
+            'hostTeam' => 'UY Scuti',
+            'homePoints' => 69,
+            'hostPoints' => 54,
+        ],
     ];
-    echo $basketChampionship[0]['homeTeam'];
-    // for ($i=0; $i < $basketChampionship; $i++){
-    //     echo '<li>Nome team: ' . $basketChampionship.[$i][0] . '</li>';
-    //     }; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,10 +63,14 @@
     <div class="container">
         <h1>Punteggi campionato</h1>
         <div class="results-div">
-            <ul class="home-team">
-                <li><?= $basketChampionship[0]['homeTeam'] . ' - ' .$basketChampionship[0]['hostTeam'] . ' | ' . 
-                $basketChampionship[0]['homePoints'] . ' - ' .
-                $basketChampionship[0]['hostPoints']?></li>
+            <ul class="list-team">
+            <?php    
+            for ($i=0; $i < count($basketChampionship); $i++) {
+            echo '<li class="match">' . $basketChampionship[$i]['homeTeam'] . ' - ' .
+                $basketChampionship[$i]['hostTeam'] . ' | ' . 
+                $basketChampionship[$i]['homePoints'] . ' - ' .
+                $basketChampionship[$i]['hostPoints'] . '</li>';
+                }?>
             </ul>
         </div>
     </div>

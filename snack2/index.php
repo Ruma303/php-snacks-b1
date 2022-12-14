7726d966d0b729bc18be3c417deabf61715edd70
Ruma <matteoh8@hotmail.it>
2022-12-14 19:31:44 +0100

    Snack2 ok

--- snack2/index.php
+++ snack2/index.php
@@ -1,3 +1,28 @@
+<?php 
+    // Acquisizione dati utente dagli input
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = $_GET['age'];
+    
+    $nameOK = false;
+    //Controllo lunghezza name
+    if (strlen($name) >= 3) {
+      $nameOK = true;
+    } 
+    //echo $nameOK . '<br>Lunghezza caratteri Nome: ' . strlen($name);
+
+    //Ricerca della chiocchiola nella mail
+    $pos_at = strpos($mail, '@');
+    //Ricerca del punto nella mail
+    $pos_dot = strpos($mail, '.', $pos_at);
+
+    $mailOK = false;
+    // Controllo se @ e . sono al loro posto ed esistono
+    if ($pos_at && $pos_dot) {
+        $mailOK = true;
+    };
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +33,24 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="container">
+        <form action="" method="GET">
+            <label for="name">Nome: </label>
+            <input type="text" id='name' name="name">
+            <label for="mail">Mail: </label>
+            <input type="mail" id='mail' name="mail">
+            <label for="age">Età: </label>
+            <input type="number" id='age' name="age">
+            <button>Invia</button>
+        </form>
+        <!-- Validazione con if se è tutto ok -->
+        <h1><?php
+            if ($nameOK && $mailOK && $age) {
+                echo 'Accesso Riuscito';
+            } else {
+                echo 'Accesso Negato';
+            }
+        ?></h1> 
+    </div>
 </body>
 </html>
